9b483d809a8d0bc7b976989e2ec2c29d20912178
Joaquin <Joaquin.Quintana@colorado.edu>
2022-07-14 09:53:45 -1000

    linked pages

--- siteMain.py
+++ siteMain.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/About")
+def about():
+    return render_template("About.html")
+
+@app.route("/skills")
+def skills():
+    return render_template("skills.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
